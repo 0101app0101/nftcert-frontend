49e73f630eb237fb624e77cd06b44df91a211137
akyp888 <akashrkashyap98@gmail.com>
2022-07-16 22:34:47 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Banner from './components/Banner';
 import Services from './components/Services';
 import Templates from './components/Templates';
 import Workwithus from './components/Workwithus';
+import NavBar from './components/NavBar';
 import './css/app.css'
 import './css/vendor/bootstrap.min.css'
 import './css/vendor/font-awesome.css'
 function App() {
   return (
     <div className="App">
-      <div style={{marginTop:"10px"}}></div>
+      <NavBar />
       <Banner />
       <Services/>
       <Templates/>
